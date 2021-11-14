1b04d9620f8ad11334d8b2286c0bffb34a22dbdc
SHRADHA YEWALE <shradhayewale1@gmail.com>
2021-11-14 11:16:27 -0800

    feat(aws):Added aws connection string.

--- ubereats-application/frontend/src/endPointUrl.js
+++ ubereats-application/frontend/src/endPointUrl.js
@@ -1,5 +1,5 @@
 const endPointObj = {
-    url: 'http://localhost:3001',
+    url: 'http://18.117.253.179:3001',
     frontendServer: ' https://s3-us-east-2.amazonaws.com/ubereatsimage'
   };
 export default endPointObj;
